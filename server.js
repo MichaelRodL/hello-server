183c6622765ee5925c6466bc11273905b86ed32b
Michael Rodriguez <michael.rodriguez@chiper.co>
2023-09-12 11:00:36 -0500

    update files hello

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('WELCOME MICHAEL');
+
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+
